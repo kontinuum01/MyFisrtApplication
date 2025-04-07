5debb2c2ca3c731944a9b5cd9d3989785ba278e9
Vladislav <kontinuum01@gmail.com>
2025-04-07 22:46:03 +0300

    Создана стартовая activity, добавлены ресурсы.

--- m5_quiz_resources/app/src/main/java/com/example/quiz/MainActivity.kt
+++ m5_quiz_resources/app/src/main/java/com/example/quiz/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.quiz.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.image.visibility = View.INVISIBLE
+            binding.image1.visibility = View.VISIBLE
+        }
     }
 }
